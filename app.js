0b90a48b59ecedb4cbb832ed69077110276084d7
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2025-09-17 12:35:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -54,17 +54,18 @@ store.on("error",()=>{
     console.log("ERROR IN MONGO SESSION STORE",err);
 })
 
-const sessionOptions={
+const sessionOptions = {
     store,
-    secret:process.env.SECRET,
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        expires:Date.now()+7*24*60*60*1000,
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
-    }
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
 };
+
 
 // app.get("/", (req, res) => {
 //     res.send("HI i am root");
